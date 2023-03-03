b2b74000c0987a2f1c675a79404a543c8b69499e
Gleb Tregubov <tregubovgleb@gmail.com>
2023-03-03 12:39:13 +0300

    complete task 1

--- task1/ViewController.swift
+++ task1/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let squareView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        squareView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(squareView)
+        
+        NSLayoutConstraint.activate([
+            view.leadingAnchor.constraint(equalTo: squareView.leadingAnchor, constant: -100),
+            view.centerYAnchor.constraint(equalTo: squareView.centerYAnchor),
+            squareView.heightAnchor.constraint(equalToConstant: 100),
+            squareView.widthAnchor.constraint(equalToConstant: 100)
+        ])
+        
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        squareView.layer.shadowRadius = 20
+        squareView.layer.shadowColor = UIColor.black.cgColor
+        squareView.layer.shadowOpacity = 0.6
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = squareView.bounds
+        gradientLayer.masksToBounds = true
+        gradientLayer.cornerRadius = 20
+        gradientLayer.colors = [UIColor.systemRed.cgColor, UIColor.systemOrange.cgColor, UIColor.systemYellow.cgColor]
+        gradientLayer.locations = [0.15, 0.65, 1]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        
+        squareView.layer.addSublayer(gradientLayer)
+        
+    }
 
 }
 
